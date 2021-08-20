5adf88196abceaaf7380f0fc4da4038361a4a214
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-08-19 18:07:24 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+class Student{
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+
+}
+
+let s1 = new Student("Sean", "Jaculina", "sophomore");
+
+console.log(s1);
+
+console.log(new Student("Emil", "Katz", "JR"));
