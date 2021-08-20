0a2e5b323696c45c6814c86a6642d6539430da47
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-08-19 18:44:04 -0700

    just messing around with instance methods

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ class Student{
         this.firstName = firstName;
         this.lastName = lastName;
         this.year = year;
+        this.tardies = 0;
+        this.scores = [];
     }
     // creating a instance method to use
     fullName(){
         return `Your full name is ${this.firstName} ${this.lastName} and you are in year ${this.year}`;
     }
+    markLate(){
+        this.tardies += 1;
+        if(this.tardies >= 3){
+            return `${this.firstName} ${this.lastName} you have been expelled`;
+        }
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`;
+    }
 
+    addScore(score){
+        this.scores.push(score);
+        return this.scores;
+    }
+    calculateAvg(){
+        let sum = this.scores.reduce(function(a, b){
+            return a+b
+        });
+        return sum/this.scores.length;
+
+    }
 }
 
 let Sean = new Student("Sean", "Jaculina", 4);
+let student2 = new Student("Student", "2", 4);
 
-console.log(Sean.fullName());
 
+Sean.addScore(91);
+Sean.addScore(82);
+console.log(Sean.calculateAvg());
